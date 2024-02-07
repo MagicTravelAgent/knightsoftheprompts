aee8b17c989298cfffdba14fa06ff6e9e189de97
Lucas Puddifoot <lucas.puddifoot@capgemini.com>
2024-02-07 11:00:59 +0100

    shift file structure

--- knight of the prompts/knightsOfThePrompts/tests.py
+++
@@ -1,9 +0,0 @@
-def test_helpers():
-    import os
-    import helpers
-
-    helpers.set_cwd()
-    helpers.ensure_requirements()
-    helpers.load_secrets(secrets_file=".env.template")
-
-    assert os.getenv("OPENAI_API_TYPE") in ("azure", "openai")
